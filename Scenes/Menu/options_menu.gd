2bc3be92df98481de53746b060c24abdf277d26a
Vallsen <valentinofischer2001@gmail.com>
2024-05-13 20:06:20 +0200

    weiter so

--- Scenes/Menu/options_menu.gd
+++ Scenes/Menu/options_menu.gd
@@ -7,3 +7,17 @@ extends Control
 
 
 
+
+
+func _on_back_to_menu_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
+
+func _ready():
+	$Music.play()
+	
+	
+	func _change_volumen(volumen, bus):
+		if volumen == 
+	
+	
+	
