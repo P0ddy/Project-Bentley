56c0023241b62c22ffcd4126679dd94eb39d876a
Vallsen <valentinofischer2001@gmail.com>
2024-05-13 22:01:27 +0200

    Working Sliders

--- Scenes/Menu/options_menu.gd
+++ Scenes/Menu/options_menu.gd
@@ -1,28 +1,17 @@
 extends Control
 
-@onready var VolumeSlider = $"Mid Color/MarginContainer/VBoxContainer/HBoxContainer/HSlider"
-@onready var MusicSlider = $"Mid Color/MarginContainer/VBoxContainer/HBoxContainer2/HSlider"
-@onready var SFXSlider = $"Mid Color/MarginContainer/VBoxContainer/HBoxContainer3/HSlider"
-
-
-
 
 
 
 func _on_back_to_menu_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
 
-func _ready():
-	$Music.play()
-	
-	
 
 
-func _change_volumen(volumen, bus):
-	if volumen == MusicSlider.min_value:
-		AudioServer.set_bus_mute(AudioServer.get_bus_index(bus), true)
+
+
+func _on_check_button_toggled(toggled_on):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
-		AudioServer.set_bus_mute(AudioServer.get_bus_index(bus), true)
-		
-			
-	
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
