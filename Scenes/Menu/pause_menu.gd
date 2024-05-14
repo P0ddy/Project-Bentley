942ab496a2c010da99acf53b0daeeefaa1331bad
Vallsen <valentinofischer2001@gmail.com>
2024-05-14 10:31:24 +0200

    gege

--- Scenes/Menu/pause_menu.gd
+++ Scenes/Menu/pause_menu.gd
@@ -2,7 +2,8 @@ class_name PauseMenu
 extends Control
 
 
-
+@export var pause_menu_options_packed_scene : PackedScene = null
+@onready var ui_panel = $Panel as CanvasLayer
 
 func _on_continue_button_button_down():
 	queue_free()
@@ -10,3 +11,10 @@ func _on_continue_button_button_down():
 
 func _on_main_menu_button_button_down():
 	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
+	
+
+func _on_options_button_pressed():
+	ui_panel.add_child(pause_options_menu)
+
+var pause_options_menu : PauseMenu = pause_menu_options_packed_scene.instantiate()
+
