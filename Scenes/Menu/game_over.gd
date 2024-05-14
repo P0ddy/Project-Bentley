5d4a857dc7b954c571f74a1c75dfbc7818923cbd
Louis Geister <geisterlouis@gmail.com>
2024-05-14 10:31:12 +0200

    add assets + reset on death

--- Scenes/Menu/game_over.gd
+++ Scenes/Menu/game_over.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _on_restart_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainScene/Game.tscn")
+	Global.score = 0
+	Global.health = 3
 
 func _on_quit_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
