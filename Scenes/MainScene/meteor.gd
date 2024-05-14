457d9daedebdde86f127c61466d40779142f49b8
Vallsen <valentinofischer2001@gmail.com>
2024-05-14 22:21:37 +0200

    Continue Meteor Hitbox

--- Scenes/MainScene/meteor.gd
+++ Scenes/MainScene/meteor.gd
@@ -34,6 +34,8 @@ func enemy_hit():
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-func _on_body_entered(body):
+
+func _on_damage_area_body_entered(body):
 	if body.has_method("player_hit"):
 		body.player_hit()
+	
