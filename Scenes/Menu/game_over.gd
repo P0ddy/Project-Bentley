0acab0b42af986a1c3b08963f5cf0b0e8541c5cb
Vallsen <valentinofischer2001@gmail.com>
2024-05-15 23:32:37 +0200

    Fixing audio for GameOverScreen

--- Scenes/Menu/game_over.gd
+++ Scenes/Menu/game_over.gd
@@ -3,10 +3,17 @@ extends Control
 
 func _on_restart_pressed():
 	AudioPlayer.button_click()
+	AudioPlayer.BossFightMusicStop()
+	AudioPlayer.InGameMusicStop()
+	AudioPlayer.InGameMusicStart()
+	
 	get_tree().change_scene_to_file("res://Scenes/MainScene/Game.tscn")
 	Global.score = 0
 	Global.health = 3
 
 func _on_quit_pressed():
+	AudioPlayer.BossFightMusicStop()
+	AudioPlayer.InGameMusicStop()
+	AudioPlayer.MainThemeStart()
 	AudioPlayer.button_click()
 	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
