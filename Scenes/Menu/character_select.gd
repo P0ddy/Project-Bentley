e28d162a495e63f40b5583f9de6c643dc886832b
Vallsen <valentinofischer2001@gmail.com>
2024-05-15 13:38:37 +0200

    Push before characterselect Changes

--- Scenes/Menu/character_select.gd
+++ Scenes/Menu/character_select.gd
@@ -5,10 +5,13 @@ func _process(delta):
 	match Global.PlayerSelect:
 		0:
 			get_node("PlayerSelect").play("Player1")
+			get_node("Selected Car").text = "Standard Karre"
 		1:
 			get_node("PlayerSelect").play("Player2")
+			get_node("Selected Car").text = "Ferrari"
 		2:
 			get_node("PlayerSelect").play("Player3")
+			get_node("Selected Car").text = "Bus"
 
 func _on_left_button_pressed():
 	if Global.PlayerSelect > 0:
@@ -18,3 +21,9 @@ func _on_left_button_pressed():
 func _on_right_button_pressed():
 	if Global.PlayerSelect < 2:
 		Global.PlayerSelect +=1
+
+
+
+func _on_select_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MainScene/Game.tscn")
+	
